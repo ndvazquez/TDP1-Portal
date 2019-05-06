74290d4f7d15c340a8e5640c8f5c2e71a0123e9b
ndvazquez <vazquez.nicolas.daniel@gmail.com>
2019-05-05 23:37:27 -0300

    Added wrappers for SDL_Window and SDL_Texture

---
+++ client/Sprite.cpp
@@ -0,0 +1,46 @@
+//
+// Created by ndv on 5/5/19.
+//
+
+#include "Sprite.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include "Window.h"
+
+Sprite::Sprite(const std::string &path, Window& window) {
+    SDL_Surface* tempSurface = nullptr;
+    tempSurface = IMG_Load(path.c_str());
+    if (tempSurface == nullptr){
+        throw SpriteInitException();
+    }
+    this->texture = SDL_CreateTextureFromSurface(window.renderer, tempSurface);
+    if (this->texture == nullptr){
+        throw SpriteInitException();
+    }
+    this->imageHeight = tempSurface->h;
+    this->imageWidth = tempSurface->w;
+    SDL_FreeSurface(tempSurface);
+}
+
+Sprite::~Sprite() {
+    this->free();
+}
+
+void Sprite::draw(Window &destWindow, int x, int y) {
+    SDL_Rect sourceRect = { 0, 0, this->imageWidth, this->imageHeight};
+    SDL_Rect destRect = {x, y ,this->imageWidth ,this->imageHeight};
+
+    destWindow.draw(*this->texture, sourceRect, destRect);
+}
+
+void Sprite::free(){
+    SDL_DestroyTexture(this->texture);
+}
+
+int Sprite::getWidth() {
+    return this->imageWidth;
+}
+
+int Sprite::getHeight() {
+    return this->imageHeight;
+}
---
+++ client/Sprite.h
@@ -0,0 +1,35 @@
+//
+// Created by ndv on 5/5/19.
+//
+
+#ifndef PORTAL_SPRITE_H
+#define PORTAL_SPRITE_H
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <exception>
+#include "Window.h"
+
+class SpriteInitException: public std::exception {
+    virtual const char* what() const throw () {
+        std::string message = "There was an error while trying to initialize a sprite: ";
+        message += SDL_GetError();
+        return message.c_str();
+    }
+};
+
+class Sprite {
+    SDL_Texture* texture;
+    int imageWidth;
+    int imageHeight;
+public:
+    Sprite(const std::string& path, Window& window);
+    ~Sprite();
+    void free();
+    void draw(Window& destWindow, int x, int y);
+    int getWidth();
+    int getHeight();
+};
+
+
+#endif //PORTAL_SPRITE_H
