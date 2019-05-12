1654f03ce06de00e4a0de56e123c6cfbbf309589
chortas <ceci.hortas@gmail.com>
2019-05-12 20:46:42 -0300

    reorder of things

---
+++ server/Stage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cecix on 12/05/19.
+//
+
+#include "Stage.h"
---
+++ server/Stage.h
@@ -0,0 +1,14 @@
+//
+// Created by cecix on 12/05/19.
+//
+
+#ifndef PORTAL_STAGE_H
+#define PORTAL_STAGE_H
+
+
+class Stage {
+
+};
+
+
+#endif //PORTAL_STAGE_H
