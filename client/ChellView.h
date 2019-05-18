18325b431705785b4c164db341fd631679690bd7
ndvazquez <vazquez.nicolas.daniel@gmail.com>
2019-05-17 23:35:15 -0300

    Added first version of Chell, our main character

---
+++ client/ChellView.h
@@ -0,0 +1,44 @@
+//
+// Created by ndv on 5/14/19.
+//
+
+#ifndef PORTAL_CHELLVIEW_H
+#define PORTAL_CHELLVIEW_H
+
+#include <unordered_map>
+#include <SDL.h>
+#include "../common/AnimatedSprite.h"
+#include <string>
+#include <yaml-cpp/yaml.h>
+
+#define TEXTURES_INFO_KEY "Chell"
+#define DEFAULT_ANIMATION "RestingIdle"
+#define CHELL_RUN "Run"
+#define CHELL_RESTING_IDLE "RestingIdle"
+
+class ChellView {
+    Window& gameWindow;
+    int chellWidth;
+    int chellHeight;
+    int xPos;
+    int yPos;
+    int xVelocity;
+    int yVelocity;
+    SDL_RendererFlip flip;
+    std::string currentAnimation;
+    std::unordered_map<std::string, AnimatedSprite*> animations;
+
+public:
+    ChellView(Window &window, int xPos, int yPos, YAML::Node texturesData);
+    ~ChellView();
+    void playAnimation(SDL_Rect& camera);
+    void handleEvent(SDL_Event& e);
+    void move(int newPosX, int newPosY, int levelWidth, int levelHeight);
+    // This method is only used by the playable Chell;
+    void updateCamera(SDL_Rect& camera, int levelWidth, int levelHeight);
+    int getPosX();
+    int getPosY();
+};
+
+
+#endif //PORTAL_CHELLVIEW_H
