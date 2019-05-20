d91b06545b419fa95f5dbcb0e1ea959ab545fed0
camiboj <camiboj@gmail.com>
2019-05-20 01:12:44 -0300

    Menu knows how to handle mouse down and up

---
+++ editor/MenuButton.cpp
@@ -0,0 +1,5 @@
+//
+// Created by camix on 18/05/19.
+//
+
+#include "MenuButton.h"
---
+++ editor/MenuButton.h
@@ -0,0 +1,16 @@
+//
+// Created by camix on 18/05/19.
+//
+
+#ifndef PORTAL_MENUBUTTON_H
+#define PORTAL_MENUBUTTON_H
+
+
+
+class MenuButton {
+
+};
+
+
+
+#endif //PORTAL_MENUBUTTON_H
