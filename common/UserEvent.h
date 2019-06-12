b4c3c1bec9916d4c00f45c2972f42f9b3081b3d6
ndvazquez <vazquez.nicolas.daniel@gmail.com>
2019-06-12 13:56:42 -0300

    Added UserEvent class to hold user event info

---
+++ common/UserEvent.h
@@ -0,0 +1,31 @@
+//
+// Created by ndv on 6/12/19.
+//
+
+#ifndef PORTAL_USEREVENT_H
+#define PORTAL_USEREVENT_H
+
+#include "../json/json.hpp"
+#include <string>
+
+class UserEvent {
+    std::string userId;
+    int eventType;
+    float userMouseX;
+    float userMouseY;
+public:
+    explicit UserEvent(nlohmann::json& jsonData);
+    UserEvent(const std::string& id,
+            int eventTypeCode,
+            float x,
+            float y);
+    ~UserEvent();
+    int getEventType();
+    float getUserPosX();
+    float getUserPosY();
+    const std::string& getUserId();
+    std::string toJsonString();
+};
+
+
+#endif //PORTAL_USEREVENT_H
