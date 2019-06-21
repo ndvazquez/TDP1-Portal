c8f5744a6db52cb6bf6171f778c42ae0c270775e
chortas <ceci.hortas@gmail.com>
2019-06-21 14:51:36 -0300

    End configuration

--- server/ConfigurationParser.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by cecix on 21/06/19.
-//
-
-#include "ConfigurationParser.h"
---
+++ server/config/configuration.cpp
@@ -0,0 +1,27 @@
+//
+// Created by cecix on 21/06/19.
+//
+
+#include <yaml-cpp/node/node.h>
+#include <yaml-cpp/yaml.h>
+#include "configuration.h"
+
+YAML::Node configuration_data = YAML::LoadFile("server/config/config.yaml");
+const struct box2DConfiguration gameConfiguration = {
+        .chellForce = configuration_data["chellForce"].as<float>(),
+        .deltaError = configuration_data["deltaError"].as<float>(),
+        .energyBallImpulseFactor = configuration_data["energyBallImpulseFactor"].as<float>(),
+        .chellInitialVelocity = configuration_data["chellInitialVelocity"].as<float>(),
+        .gravityJump = configuration_data["gravityJump"].as<float>(),
+        .gravity = configuration_data["gravity"].as<float>(),
+        .rockForce = configuration_data["rockForce"].as<float>(),
+        .elevationForce = configuration_data["elevationForce"].as<float>(),
+        .rockFriction = configuration_data["rockFriction"].as<float>(),
+        .deathTimeEnergyBall = configuration_data["deathTimeEnergyBall"].as<float>(),
+        .delayEnergyTransmitter = configuration_data["delayEnergyTransmitter"].as<float>(),
+        .offsetEnergyTransmitter = configuration_data["offsetEnergyTransmitter"].as<float>(),
+        .velocityDownload = configuration_data["velocityDownload"].as<float>(),
+        .velocityRelease = configuration_data["velocityRelease"].as<float>(),
+        .directionFactor = configuration_data["directionFactor"].as<float>(),
+        .shotFactor = configuration_data["shotFactor"].as<float>()
+};
