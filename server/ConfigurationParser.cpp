33a5c083f0ab2b4ce0df33cbc9c0f77c0f96f0b6
chortas <ceci.hortas@gmail.com>
2019-06-21 11:48:24 -0300

    Initial support to config file

---
+++ server/ConfigurationParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cecix on 21/06/19.
+//
+
+#include "ConfigurationParser.h"
---
+++ server/ConfigurationParser.h
@@ -0,0 +1,13 @@
+//
+// Created by cecix on 21/06/19.
+//
+
+#ifndef PORTAL_CONFIGURATIONPARSER_H
+#define PORTAL_CONFIGURATIONPARSER_H
+
+
+class ConfigurationParser {
+
+};
+
+#endif //PORTAL_CONFIGURATIONPARSER_H
