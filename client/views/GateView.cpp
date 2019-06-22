32864371560fcb2b73e55e908eac448430e1c6d2
ndvazquez <vazquez.nicolas.daniel@gmail.com>
2019-06-22 04:23:09 -0300

    Added sprite sheet for GateView, it should have a proper animation now

--- client/views/GateView.cpp
+++ client/views/GateView.cpp
@@ -9,12 +9,17 @@ GateView::GateView(Window &window, int xPos, int yPos,
         View(window, xPos, yPos, factor, GATE_WIDTH, GATE_HEIGHT){
     const YAML::Node& spriteData = texturesData[TEXTURES_GATE_KEY];
     std::string path = spriteData["path"].as<std::string>();
-    sprite = new Sprite(path, window);
+    int frames = spriteData["frames"].as<int>();
+    animation = new AnimatedSprite(path, window, frames);
+    previousState = GATE_CLOSED;
     currentState = GATE_CLOSED;
+    transitionAnimation = false;
+    frameCounter = 1;
+    lastFrame = frames * TRANSITION_MULTIPLIER;
 }
 
 GateView::~GateView() {
-    delete sprite;
+    delete animation;
 }
 
 void GateView::playAnimation(const SDL_Rect &camera) {
@@ -23,9 +28,22 @@ void GateView::playAnimation(const SDL_Rect &camera) {
                            viewPosY - camera.y,
                            int(viewWidthInMeters * mtpFactor),
                            int(viewHeightInMeters * mtpFactor)};
-    sprite->draw(&destRect);
+    animation->draw(&destRect);
+    if (transitionAnimation) {
+        animation->updateFrameStep();
+        ++frameCounter;
+        if (frameCounter ==  lastFrame) {
+            transitionAnimation = false;
+            frameCounter = 1;
+            animation->reverseAnimation();
+        }
+    }
 }
 
 void GateView::setState(int state) {
+    previousState = currentState;
     currentState = state;
+    if (previousState != currentState) {
+        transitionAnimation = true;
+    }
 }
