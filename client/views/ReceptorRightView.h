ef51656267bce8ceb7ec67dcd75247d873ef3cd4
ndvazquez <vazquez.nicolas.daniel@gmail.com>
2019-06-23 23:28:10 -0300

    Added ReceptorRightView

---
+++ client/views/ReceptorRightView.h
@@ -0,0 +1,36 @@
+//
+// Created by ndv on 6/23/19.
+//
+
+#ifndef PORTAL_RECEPTORRIGHTVIEW_H
+#define PORTAL_RECEPTORRIGHTVIEW_H
+
+
+#include "View.h"
+#include "../../common/Sprite.h"
+#include <unordered_map>
+#include <string>
+#include <yaml-cpp/yaml.h>
+#include "../../common/constants.h"
+
+#define TEXTURES_ER_RIGHT_KEY "ReceptorRight"
+#define RECEPTOR_STATE_ON 1
+#define RECEPTOR_STATE_OFF 0
+
+class ReceptorRightView : public View{
+    int receptorState;
+    std::unordered_map<int, Sprite*> sprites;
+public:
+    ReceptorRightView(Window& window,
+                     int xPos,
+                     int yPos,
+                     int factor,
+                     YAML::Node texturesData);
+    ~ReceptorRightView();
+    void setState(int state);
+    void playAnimation(const SDL_Rect& camera);
+};
+
+
+
+#endif //PORTAL_RECEPTORRIGHTVIEW_H
