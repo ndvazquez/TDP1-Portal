e0c00c0d03f6e4535c77537b722b68277b07a22c
ndvazquez <vazquez.nicolas.daniel@gmail.com>
2019-06-25 03:26:13 -0300

    Added install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+echo "Creating build directory.."
+rm -rf build
+mkdir build
+cd build
+echo "Building project..."
+cmake ..
+echo "Linking project files..."
+make Client
+make Server
+make Editor
+cd ..
+echo "Installing game files to chosen directory..."
+mkdir -p $1
+cp Client $1
+cp Server $1
+cp Editor $1
+cp font.ttf $1
+cp -r resources $1
+cp -r config $1
+cp -r levels $1
+echo "Cleaning build files..."
+rm Client
+rm Server
+rm Editor
+rm -rf build
+echo "Installation finished, enjoy our game!"
